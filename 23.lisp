7895d563aeb590f191332ded1b1ccf4126976412
Svante v. Erichsen <svante.v.erichsen@web.de>
2018-12-31 11:25:04 +0100

    wip 23: walking to the solution area (doesn't work)

--- 23.lisp
+++ 23.lisp
@@ -3,6 +3,7 @@
 (defpackage #:aoc-2018-23
   (:use #:cl
         #:alexandria
+        #:aoc-2018
         #:arrows
         #:cl-ppcre
         #:let-plus))
@@ -40,82 +41,149 @@
           :count (<= (manhattan-distance strongest bot)
                      (nanobot-radius strongest)))))
 
-(defun manhattan-distance (a b)
-  (declare (nanobot a b))
-  (loop :for i :across (nanobot-pos a)
-        :and j :across (nanobot-pos b)
+(defgeneric manhattan-distance (a b))
+
+(defmethod manhattan-distance ((a nanobot) (b nanobot))
+  (manhattan-distance (nanobot-pos a) (nanobot-pos b)))
+
+(defmethod manhattan-distance ((a vector) (b nanobot))
+  (manhattan-distance a (nanobot-pos b)))
+
+(defmethod manhattan-distance ((a nanobot) (b vector))
+  (manhattan-distance (nanobot-pos a) b))
+
+(defmethod manhattan-distance ((a vector) (b vector))
+  (loop :for i :across a
+        :and j :across b
         :sum (abs (- i j))))
 
 ;;; Part 2
 
 ;; The goal here is to find clusters of bots whose radii all mutually overlap.
-;; I see this as a connected subgraph problem.
+;; I see this as a connected subgraph problem.  This is followed by walking to
+;; that full overlap region, then walking towards the origin.
 
-(defvar *nanobots*)
-(defvar *adjacency-matrix*)
+(defun aoc23b (&optional (nanobots (read-nanobots-from-file)))
+  (let+ ((adjacency-matrix (adjacency-matrix nanobots))
+         ((&values &ign) (visualize-adjacency-matrix adjacency-matrix "23adj"))
+         ;; Looking at that matrix, there seem to be a few outliers, while most
+         ;; bots overlap.  Quantify that:
+         (overlap-counts
+          (frequencies (map 'vector #'logcount adjacency-matrix)))
+         ;; Yeah, we can eliminate everything below 900 (actually 981, but the
+         ;; most overlapping outlier has 607).
+         ((&values reduced-matrix blacklist)
+          (eliminate-outliers adjacency-matrix 900))
+         ;; Taking a look.
+         ((&values &ign) (visualize-adjacency-matrix reduced-matrix "23noo"))
+         ;; Seems to be white.  Only one frequency left:
+         (reduced-overlap-counts
+          (frequencies (map 'vector #'logcount reduced-matrix)))
+         (octahedron-count (-> reduced-overlap-counts
+                               hash-table-alist
+                               caar))
+         ;; Don't need blacklist anymore, so destroy it.
+         (bots (loop :for i :upfrom 0
+                     :for bot :across nanobots
+                     :if (some-> blacklist first (= i))
+                       :do (pop blacklist)
+                     :else
+                       :collect bot))
+         ((&values &ign)
+          (progn
+            (assert (> (hash-table-count overlap-counts) 1))
+            (assert (= (hash-table-count reduced-overlap-counts) 1))
+            (assert (= (length bots) octahedron-count))))
+         ;; The full overlap region must be convex, by the way.
+         (some-full-overlap-pos
+          (loop :for pos := (weighted-middle bots)
+                  :then (step-pos pos
+                                  (weighted-middle missing)
+                                  (reduce #'max distances))
+                :for (missing distances)
+                  := (loop :for bot :in bots
+                           :for missing-distance
+                             := (- (manhattan-distance pos bot)
+                                   (nanobot-radius bot))
+                           :when (plusp missing-distance)
+                             :collect bot :into missing
+                             :and :collect missing-distance :into distances
+                           :finally (return (list missing distances)))
+                :until (endp missing)
+                :do (print (list :distances distances
+                                 :length (length distances)))
+                    (break)
+                :finally (return pos))))
+    some-full-overlap-pos))
 
-(defstruct cluster
-  (bots 0 :type integer)
-  (candidates 0 :type integer))
-
-(defun aoc23b (&optional (*nanobots* (read-nanobots-from-file)))
-  (let* ((*adjacency-matrix* (adjacency-matrix *nanobots*))
-         ;; A cluster is represented as a bit field of nanobots in this cluster.
-         #+debug
-         (largest-clusters
-           (loop :for clusters
-                   := (loop :for i :below (length *nanobots*)
-                            :collect (create-cluster (ash 1 i)))
-                     :then new-clusters
-                 :for new-clusters := (mapcan #'extend-cluster clusters)
-                 :while new-clusters
-                 :finally (return clusters))))
-    (visualize-adjacency-matrix *adjacency-matrix*)))
-
-(defun visualize-adjacency-matrix (m)
-  (let ((image (opticl:make-8-bit-gray-image 1000 1000 :initial-element 0)))
-    (dotimes (row 1000)
-      (dotimes (col 1000)
-        (setf (opticl:pixel image row col)
-              (* 255 (ldb (byte 1 col) (aref m row))))))
-    (opticl:write-png-file "/tmp/23.png" image)))
-
-(defun extend-cluster (cluster)
-  (let+ (((&structure cluster- bots candidates) cluster))
-    (loop :for i :below (integer-length candidates)
-          :for next-candidates
-            := (when (logbitp i candidates)
-                 (logand candidates
-                         (aref *adjacency-matrix* i)))
-          :when next-candidates
-            :collect (make-cluster :bots (logior bots (ash 1 i))
-                                   :candidates next-candidates))))
-
-(defun create-cluster (bot-indicator)
-  (make-cluster :bots bot-indicator
-                :candidates (candidate-mask bot-indicator)))
-
-(defun candidate-mask (bots-bitfield)
-  "Returns a bitfield integer indicating every bot not indicated by
-BOTS-BITFIELD whose radius overlaps every bot in BOTS-BITFIELD."
-  ;; TODO: das geht schneller -> nur die 1en ansehen
-  (let ((adjacencies (loop :for i :below (integer-length bots-bitfield)
-                           :when (logbitp i bots-bitfield)
-                             :collect (aref *adjacency-matrix* i))))
-    (reduce #'logand adjacencies)))
+(defun step-pos (start goal distance)
+  (pos-round (pos+ start
+                   (pos* distance (normalize (pos- goal start))))))
 
 (defun adjacency-matrix (bots)
   (map-into (make-array (length bots)
                         :element-type 'integer)
-            (lambda (bot i)
+            (lambda (bot)
               (loop :for other :across bots
                     :for j :upfrom 0
-                    :for bit := (if (and (/= i j)
-                                         (<= (manhattan-distance bot other)
-                                             (+ (nanobot-radius bot)
-                                                (nanobot-radius other))))
+                    :for bit := (if (<= (manhattan-distance bot other)
+                                        (+ (nanobot-radius bot)
+                                           (nanobot-radius other)))
                                     1
                                     0)
                     :sum (ash bit j)))
-            bots
-            (iota (length bots))))
+            bots))
+
+(defun visualize-adjacency-matrix (m name &aux (size (length m)))
+  (let ((image (opticl:make-8-bit-gray-image size size :initial-element 0)))
+    (dotimes (row size)
+      (dotimes (col size)
+        (setf (opticl:pixel image row col)
+              (* 255 (ldb (byte 1 col) (aref m row))))))
+    (opticl:write-png-file (format nil "/tmp/~a.png" name) image)))
+
+(defun eliminate-outliers (m count)
+  (let+ (((&values keep blacklist)
+          (loop :for i :from 0
+                :for adj :across m
+                :if (> (logcount adj) count)
+                  :collect adj :into keep
+                :else
+                  :collect i :into blacklist
+                :finally (return (values keep blacklist))))
+         (bites (loop :for (start-1 b-end) :on (cons -1 blacklist)
+                      :for start := (1+ start-1)
+                      :for end := (or b-end 1000)
+                      :collect (byte (- end start) start))))
+    (values (map 'vector
+                 (lambda (adj)
+                   (loop :for bite :in bites
+                         :for i :upfrom 0
+                         :sum (ash (ldb bite adj) (- (byte-position bite) i))))
+                 keep)
+            blacklist)))
+
+(defun weighted-middle (bots)
+  (loop :for bot :in bots
+        :for weight := (/ (nanobot-radius bot))
+        :for weighted-pos := #1=(pos* weight (nanobot-pos bot))
+          :then (pos+ weighted-pos #1#)
+        :sum weight :into weight-sum
+        :finally (return (pos-round (pos* (/ weight-sum) weighted-pos)))))
+
+(defun pos* (scalar pos)
+  (map 'vector (curry #'* scalar) pos))
+
+(defun pos+ (pos-a pos-b)
+  (map 'vector #'+ pos-a pos-b))
+
+(defun pos- (pos-a pos-b)
+  (map 'vector #'- pos-a pos-b))
+
+(defun pos-round (pos)
+  (map 'vector #'round pos))
+
+(defun normalize (pos)
+  (let ((length (sqrt (loop :for c :across pos
+                            :sum (* c c)))))
+    (pos* (/ length) pos)))
